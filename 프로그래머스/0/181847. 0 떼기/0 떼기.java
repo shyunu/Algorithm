a22a65745810eaaf912366995e7af557c67f8440
shyunu <ksh020674@gmail.com>
2024-12-08 19:45:52 +0900

    [level 0] Title: 0 떼기, Time: 0.05 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String n_str) {
+
+        int str = Integer.parseInt(n_str);
+        String answer = String.valueOf(str);
+        return answer;
+        
+    }
+}
