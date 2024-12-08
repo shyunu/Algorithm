3c63e89ced263ef97b1d0f07e5859bf6d4a9fcb5
shyunu <ksh020674@gmail.com>
2024-12-08 19:09:57 +0900

    [Bronze V] Title: A+B - 7, Time: 268 ms, Memory: 19028 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+
+
+public class Main {
+
+
+
+	 public static void main(String[] args) {
+
+
+		 
+
+
+		 Scanner scan = new Scanner(System.in);
+
+
+		 int t = scan.nextInt();
+
+
+		 String result = "";
+
+
+		 
+
+
+		 for ( int i = 0; i < t; i++) {
+
+
+			 int a = scan.nextInt();
+
+
+			 int b = scan.nextInt();
+
+
+			 result += "Case #" + (i+1) + ": " + (a + b)+ " ";
+
+
+		 }
+
+
+		 System.out.println(result);
+
+
+	 }
+
+
+	 
+
+
+}
