caa6b52f36689c3fdc24e4df4b8f1e8ee9d04a9f
shyunu <ksh020674@gmail.com>
2024-12-08 19:07:12 +0900

    [Bronze V] Title: 나머지, Time: 204 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		
+		System.out.println((a+b)%c);
+		System.out.println(((a%c) + (b%c))%c);
+		System.out.println((a*b)%c);
+		System.out.println(((a%c)*(b%c))%c);
+		
+		scan.close();
+	}
+}
