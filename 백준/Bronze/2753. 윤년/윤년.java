e2c570ba6e9b263a580c2ec4a9f358d91fcefe99
shyunu <ksh020674@gmail.com>
2024-12-08 19:04:25 +0900

    [Bronze V] Title: 윤년, Time: 212 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int i = scan.nextInt();
+		
+		if ( i % 4 == 0 & i % 100 != 0 | i % 400 == 0) {
+			System.out.println(1);
+		} else {
+			System.out.println(0);
+		}
+		
+		scan.close();
+		
+	}
+}
