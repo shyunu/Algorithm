8ecf229048259648494bcd5e5dfd9d5f679d4b81
shyunu <ksh020674@gmail.com>
2024-12-09 17:46:55 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,16 @@
+-- 코드를 입력하세요
+SELECT
+    b.title,
+    b.board_id,
+    r.reply_id,
+    r.writer_id,
+    r.contents,
+    DATE_FORMAT(r.created_date, '%Y-%m-%d') AS created_date
+FROM
+    used_goods_board AS b
+    JOIN used_goods_reply AS r
+    ON b.board_id=r.board_id
+WHERE 
+    DATE_FORMAT(b.created_date, '%Y-%m')='2022-10'
+ORDER BY 
+    6, 1;
