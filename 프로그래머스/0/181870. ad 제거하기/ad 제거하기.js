c2a1c6e49555814743c371e67a4e248bd9d23a4f
shyunu <ksh020674@gmail.com>
2025-03-20 14:27:04 +0900

    [level 0] Title: ad 제거하기, Time: 0.36 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.js
@@ -0,0 +1,11 @@
+function solution(strArr) {
+    var answer = [];
+    
+    for(let i of strArr) {
+        if(!i.includes('ad')) {
+            answer.push(i);
+        }
+    }
+    
+    return answer;
+}
