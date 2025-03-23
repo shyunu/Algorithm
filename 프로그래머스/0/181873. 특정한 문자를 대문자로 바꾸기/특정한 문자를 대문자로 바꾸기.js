5af9758f35611b44de421ee7ddeb66846c217d05
shyunu <ksh020674@gmail.com>
2025-03-23 18:14:13 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -0,0 +1,13 @@
+function solution(my_string, alp) {
+    var answer = '';
+    
+    for(let i of my_string) {
+        if(i == alp) {
+            answer += alp.toUpperCase();
+        } else {
+            answer += i;
+        }
+    }
+    
+    return answer;
+}
