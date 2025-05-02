9d31af219b9ec68053e1461b23b4197a6336c190
shyunu <ksh020674@gmail.com>
2025-05-02 16:27:31 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 5.58 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -0,0 +1,22 @@
+function solution(arr, queries) {
+    var answer = [];
+    
+    for(let q of queries) {
+        const [s, e, k] = q;
+        let min = Infinity; // 최소값
+        
+        for(let i = s; i <= e; i++) {
+            if(arr[i] > k && arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        
+        answer.push(min == Infinity ? -1 : min);
+    }
+    
+    
+    
+
+    
+    return answer;
+}
