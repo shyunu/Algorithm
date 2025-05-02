3ba352e060053060754be804e2e6e253f6d7b3bd
shyunu <ksh020674@gmail.com>
2025-05-02 16:51:16 +0900

    [level 0] Title: 배열 만들기 6, Time: 10.20 ms, Memory: 63.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+    var stk = [];
+    
+    for(let i = 0; i < arr.length; i++) {
+        if(stk.length < 1) {
+            stk.push(arr[i]);
+        } else {
+            if(stk[stk.length-1] == arr[i]) {
+                stk.pop();
+            } else {
+                stk.push(arr[i]);
+            }
+        }
+    }
+    
+    return stk.length < 1 ? [-1] : stk;
+}
