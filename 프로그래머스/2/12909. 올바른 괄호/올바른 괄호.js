b538c8cf52a7ccd2bb6bb7433cc94bceb655da8a
shyunu <ksh020674@gmail.com>
2025-05-04 12:25:32 +0900

    [level 2] Title: 올바른 괄호, Time: 3.65 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,16 @@
+function solution(s){
+    let cnt = 0; 
+    
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] === "(") {
+            cnt++;
+        } else {
+            cnt--;
+        }
+        
+        if(cnt < 0) return false;
+    }
+    
+    return cnt === 0;
+    
+}
