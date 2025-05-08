2012976131c50307a8a902d4cc597eaebf055fd1
shyunu <ksh020674@gmail.com>
2025-05-08 13:04:02 +0900

    [level 0] Title: 조건 문자열, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,17 @@
+function solution(ineq, eq, n, m) {
+    
+    if(ineq == ">") {
+        if(eq == "=") {
+            return n >= m ? 1 : 0;
+        } else {
+            return n > m ? 1 : 0;
+        }
+    } else {
+        if(eq == "=") {
+            return n <= m ? 1 : 0;
+        } else {
+            return n < m ? 1 : 0;
+        }
+    }
+    
+}
