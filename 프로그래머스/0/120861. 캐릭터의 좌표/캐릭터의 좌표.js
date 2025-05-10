f7f5d6f117fdebfc419f29394f6273cfd112d36a
shyunu <ksh020674@gmail.com>
2025-05-10 19:51:02 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,15 +1,14 @@
 function solution(keyinput, board) {
     const answer = [0, 0]; // 시작 위치
-
-    const xLimit = Math.floor(board[0] / 2);
-    const yLimit = Math.floor(board[1] / 2);
-
-    for (let key of keyinput) {
-        if (key === "up" && answer[1] < yLimit) answer[1]++;
-        else if (key === "down" && answer[1] > -yLimit) answer[1]--;
-        else if (key === "left" && answer[0] > -xLimit) answer[0]--;
-        else if (key === "right" && answer[0] < xLimit) answer[0]++;
+    const maxX = Math.floor(board[0] / 2);
+    const maxY = Math.floor(board[1] / 2);
+    
+    for (let i of keyinput) {
+        if (i === "up" && answer[1] < maxY) answer[1]++;
+        else if (i === "down" && answer[1] > -maxY) answer[1]--;
+        else if (i === "left" && answer[0] > -maxX) answer[0]--;
+        else if (i === "right" && answer[0] < maxX) answer[0]++;
     }
-
+    
     return answer;
 }
