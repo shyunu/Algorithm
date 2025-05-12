9d45dff95ec4609fcd9ca4162dd52eeb1975ec49
shyunu <ksh020674@gmail.com>
2025-05-12 15:37:36 +0900

    [level 1] Title: K번째수, Time: 5.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,12 @@
+function solution(array, commands) {
+    const answer = [];
+    
+    for(let i = 0; i < commands.length; i++) {
+        const arr = array.slice(commands[i][0]-1, commands[i][1]);
+        arr.sort((a,b) => a-b);
+        console.log(arr);
+        answer.push(arr[commands[i][2]-1]);
+    }
+    
+    return answer;
+}
