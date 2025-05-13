8bdbb0f4175d52dfff3addbf0efd1618d6f21e0f
shyunu <ksh020674@gmail.com>
2025-05-13 14:09:37 +0900

    [level 0] Title: 다항식 더하기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,7 +1,7 @@
 function solution(polynomial) {
-    let num = 0; // 상수항
-    let numX = 0; // x항 계수
-    let isAdd = true;
+    let num = 0; // 상수항 연산
+    let numX = 0; // 일차항 연산 (x의 계수)
+    let isAdd = true; // 참이면 +, 거짓이면 -
     const polyArr = polynomial.split(" ");
     
     for (let p = 0; p < polyArr.length; p++) {
@@ -9,27 +9,36 @@ function solution(polynomial) {
         
         if (token === "+") {
             isAdd = true;
-        } else if (token === "-") {
+        }
+        else if (token === "-") {
             isAdd = false;
-        } else {
+        }
+        else {
             if (token.includes("x")) {
-                const coef = token === "x" ? 1 : parseInt(token.slice(0, -1));
-                if (isAdd) numX += coef;
-                else numX -= coef;
+                const coef = token.length === 1 ? 1 : parseInt(token.slice(0, -1));
+                if (isAdd) {
+                    numX += coef;
+                } else {
+                    numX -= coef;
+                }
             } else {
                 const value = parseInt(token);
-                if (isAdd) num += value;
-                else num -= value;
+                if (isAdd) {
+                    num += value;
+                }
+                else {
+                    num -= value;
+                }
             }
         }
     }
-
-    // 결과 조립
-    if (numX !== 0 && num !== 0) {
+    
+    if (num !== 0 && numX !== 0) {
         return (numX === 1 ? "x" : numX + "x") + " + " + num;
     } else if (numX !== 0) {
-        return numX === 1 ? "x" : numX + "x";
+        return (numX === 1 ? "x" : numX + "x");
     } else {
         return num.toString();
     }
+    
 }
