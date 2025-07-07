c5be4689ef95b64024c0ff23d8bcb3e886819fab
shyunu <ksh020674@gmail.com>
2025-07-07 19:29:58 +0900

    Time: 37 ms (22.99%), Space: 53.6 MB (93.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -2,7 +2,7 @@ var twoSum = function(nums, target) {
     let idxArr = [];
 
     for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = 1; j < nums.length; j++) {
+        for (let j = i + 1; j < nums.length; j++) {
             if(nums[i] + nums[j] == target) {
                 idxArr.push(i, j);
                 return idxArr;
