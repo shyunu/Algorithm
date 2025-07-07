d0f6dec8408a76a7c8ae347ddc85f6438ca95947
shyunu <ksh020674@gmail.com>
2025-07-07 19:04:21 +0900

    Time: 0 ms (100%), Space: 53.1 MB (92.42%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,15 @@
+var longestCommonPrefix = function(strs) {
+    if (!strs.length) return "";
+
+    strs.sort();
+
+    let first = strs[0];
+    let last = strs[strs.length - 1];
+    let i = 0;
+
+    while(i < first.length && first[i] === last[i]) {
+        i++;
+    }
+
+    return first.slice(0, i);    
+};
