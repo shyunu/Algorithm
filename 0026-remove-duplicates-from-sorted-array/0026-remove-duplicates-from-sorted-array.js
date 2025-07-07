8366c9c29e35dcdd8153243ffc4a2b9a75043d82
shyunu <ksh020674@gmail.com>
2025-07-07 19:31:15 +0900

    Time: 1 ms (59.28%), Space: 56.1 MB (93.23%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+var removeDuplicates = function(nums) {
+    if(nums.length === 0) return 0;
+
+    let insertPos = 1;
+
+    for(let i = 1; i < nums.length; i++) {
+        if(nums[i] !== nums[i-1]) {
+            nums[insertPos] = nums[i];
+            insertPos++;
+        }
+    }
+
+    return insertPos;
+};
