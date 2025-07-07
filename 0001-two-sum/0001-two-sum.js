ca6fc8ee385331741f7614728290858ae656ef1e
shyunu <ksh020674@gmail.com>
2025-07-07 18:56:57 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,14 @@
+var twoSum = function(nums, target) {
+    let idxArr = [];
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = 1; j < nums.length; j++) {
+            if(nums[i] + nums[j] == target) {
+                idxArr.push(i, j);
+                return idxArr;
+            }
+        }
+    }
+
+    
+};
