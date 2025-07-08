3268b6bde09a4c7de01ba71d2c7d70d5c0c7c92a
shyunu <ksh020674@gmail.com>
2025-07-08 15:34:59 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+    nums.sort((a, b) => a - b);
+
+    const n = nums.length;
+
+    const case1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
+    const case2 = nums[0] * nums[1] * nums[2];
+
+    return Math.max(case1, case2);
+};
